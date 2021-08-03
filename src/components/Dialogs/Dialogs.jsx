56cbc44c09d645eeb1a70615f9772d1a4882d77c
deathdeathdeathbed <88399115+deathdeathdeathbed@users.noreply.github.com>
2021-08-03 22:26:04 +0300

    1s
    1 s

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -18,16 +18,41 @@ const Dialogs = (props) => {
         props.sendMessage(values.newMessageBody);
     }
 
+<<<<<<< HEAD
     if (!props.isAuth) return <Redirect to={"/login"} /> ;
+=======
+    let dialogsData = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrew'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'}
+    ]
+
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your it-kamasutra?'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yo'}
+    ]
+>>>>>>> parent of 36f692b (25: map)
 
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                { dialogsElements }
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
             </div>
             <div className={s.messages}>
+<<<<<<< HEAD
                 <div>{ messagesElements }</div>
 
+=======
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+>>>>>>> parent of 36f692b (25: map)
             </div>
             <AddMessageForm onSubmit={addNewMessage} />
         </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,6 +7,7 @@ import {Textarea} from "../../common/FormsControls/FormsControls";
 
 const maxLength10 = maxLengthCreator(10);
 
+<<<<<<< HEAD
 let AddNewPostForm = (props) => {
     return <form onSubmit={props.handleSubmit}>
         <div>
@@ -33,12 +34,24 @@ const MyPosts = React.memo(props => {
         props.addPost(values.newPostText);
     }
 
+=======
+    let postData = [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'It\'s my first post', likesCount: 11}
+    ]
+
+>>>>>>> parent of 36f692b (25: map)
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <AddNewPostFormRedux onSubmit={onAddPost}/>
             <div className={s.posts}>
+<<<<<<< HEAD
                 {postsElements}
+=======
+                <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
+                <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
+>>>>>>> parent of 36f692b (25: map)
             </div>
         </div>
     )
